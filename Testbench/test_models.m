64ce2daa2d29e8046182b0ab95429c174aad3c65
Davidxxx1990 <DavidJammer@gmx.de>
2022-06-09 15:15:12 +0200

    Add files via upload

---
+++ Testbench/test_models.m
@@ -0,0 +1,8 @@
+function tests = test_models
+	tests = functiontests(localfunctions);
+end
+
+%Run the models in the Examplex directory and compare the results 
+%with the saved results.
+%
+%run with: run(test_models)
